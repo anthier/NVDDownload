722b9d527bcaf8b68e9c64b920da271f3a0020c1
anthier <agautier9@gmail.com>
2025-12-05 15:35:09 -0800

    Update help text to match the tags formatter output.

--- __main__.py
+++ __main__.py
@@ -59,7 +59,7 @@ def main():
         '--formatters',
         type=comma_separated_list,
         help='list of formatters (comma-separated) to apply to raw JSON columns (these ignore lf parsing settings):\n'
-             '- tags: output tags separated by commas\n'
+             '- tags: output tags in "<tag>: <source 1>, <source n>" format, one per line\n'
              '- references: output references in "<reference>: <source 1>, <source n> (<tag 1>, <tag n>)" format, one per line\n'
              '- weaknesses: output weaknesses in "<weakness>: <source 1>, <source n>" format\n'
              '- configurations: output configurations with operators, one per line\n'
